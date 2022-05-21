09cdd1931719813b5d66d5adaa0dc8c6b00b6d1c
Rosiele David <rosieleserpa@hotmail.com>
2022-05-21 19:47:40 -0300

    atualiza forma de conexão com o mongo

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 const express = require("express");
 const routes = require("./routes");
 const app = express();
+const database = require('./models/connection');
 
 app.use(express.json());
 app.use(routes);
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-console.log(`Ouvindo a porta ${PORT}`);
-});
+database.connect().then(() => {
+  app.listen(PORT, () => {
+    console.log(`Ouvindo a porta ${PORT}`);
+  });
+})
