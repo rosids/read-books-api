2144c852b693367f924e5c5fffe9b1b8cb8e1ce1
Rosiele David <rosieleserpa@hotmail.com>
2022-05-21 18:53:31 -0300

    remove schemas

--- src/schemas/BookSchema.js
+++
@@ -1,8 +0,0 @@
-const Schema = require('mongoose').Schema;
-
-const bookSchema = new Schema({
-  name: { type: String, required: true },
-  comment: { type: String}
-});
-
-module.exports = bookSchema;
