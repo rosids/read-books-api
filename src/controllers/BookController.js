48f0e705799f7b1a2108c714f19495a8bac6f712
Rosiele David <rosieleserpa@hotmail.com>
2022-05-21 19:28:47 -0300

    remove console.log de debug do controller

--- src/controllers/BookController.js
+++ src/controllers/BookController.js
@@ -2,7 +2,6 @@ const getAllBooks = require("../services/BookService");
 
 const getAll = async (_req, res) => {
   const books = await getAllBooks();
-  console.log('controller');
   res.status(200).send(books);
 };
 
