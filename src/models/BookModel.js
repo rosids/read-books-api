fdf26fe67e0a349b5e20fd4008a3435eab5950bc
Rosiele David <rosieleserpa@hotmail.com>
2022-05-24 17:48:18 -0300

    add model de criação de livro

--- src/models/BookModel.js
+++ src/models/BookModel.js
@@ -1,14 +1,25 @@
 const { model, Schema } = require('mongoose');
 
 const bookSchema = new Schema({
-  name: { type: String, required: true },
+  name: { type: String, required: [true, 'O nome é obrigatório'] },
   comment: { type: String},
 });
 
+const bookModel = model('books', bookSchema);
+
 const getBooks = async () =>  {
-  const bookModel = await model('books', bookSchema, 'books');
   const books = await bookModel.find().lean(); // lean = boa prática de performance, para retornar um JSON text-plain ao invés de objetos Mongoose complexos
   return books;
 };
 
-module.exports = getBooks;
+const createBook = async (item) => {
+  const book = new bookModel(item);
+  const newBook = await book.save();
+  return newBook;
+}
+
+module.exports = {
+  bookModel,
+  getBooks,
+  createBook,
+};
