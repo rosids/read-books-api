526c2bb108f2e8de7e68e4056f31ba640faa48a5
Rosiele David <rosieleserpa@hotmail.com>
2022-05-24 20:37:49 -0300

    add rota de deletar livro

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,7 @@
 const Router  = require('express').Router;
 const routes = Router();
 
-const { getAll, getId, createBook } = require('../controllers/BookController');
+const { getAll, getId, createBook, deleteBook } = require('../controllers/BookController');
 const { validateBook } = require('../middlewares');
 
 routes.get('/books/:id', getId);
@@ -9,5 +9,7 @@ routes.get('/books/:id', getId);
 routes.get('/books', getAll);
 
 routes.post('/books', validateBook, createBook);
+
+routes.delete('/books/:id', deleteBook);
 
 module.exports =  routes;
