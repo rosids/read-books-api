83a92f6e6da64a482cb49b9df8a7544c9ee79853
Rosiele David <rosieleserpa@hotmail.com>
2022-05-24 17:49:55 -0300

    add rota de criação de livro

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,11 @@
 const Router  = require('express').Router;
 const routes = Router();
 
-const getAll = require('../controllers/BookController');
+const { getAll, createBook } = require('../controllers/BookController');
+const { validateBook } = require('../middlewares');
 
 routes.get('/books', getAll);
+
+routes.post('/books', validateBook, createBook);
 
 module.exports =  routes;
