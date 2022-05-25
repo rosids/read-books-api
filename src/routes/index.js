bfbda9d7d71930ccc4ded235a78982685de9cf6b
Rosiele David <rosieleserpa@hotmail.com>
2022-05-25 15:14:49 -0300

    add rota de editar livro

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,14 +1,16 @@
 const Router  = require('express').Router;
 const routes = Router();
 
-const { getAll, getId, createBook, deleteBook } = require('../controllers/BookController');
-const { validateBook } = require('../middlewares');
+const { getAll, getId, createBook, deleteBook, updatedBook } = require('../controllers/BookController');
+const { validateBook, validateBookUpdate } = require('../middlewares');
 
 routes.get('/books/:id', getId);
 
 routes.get('/books', getAll);
 
 routes.post('/books', validateBook, createBook);
+
+routes.patch('/books/:id', validateBookUpdate, updatedBook);
 
 routes.delete('/books/:id', deleteBook);
 
